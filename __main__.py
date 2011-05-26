f76265ac871a85687ddcc5500c027011dcff1ee1
Ed L <fiddlerwoa.roof+github@gmail.com>
2011-05-26 17:03:01 -0500

    added a main module

---
+++ __main__.py
@@ -0,0 +1,12 @@
+from . import proxy
+import optparse
+
+def main(host, path=None):
+	proxy_ = proxy.JSONRPCProxy(host, path)
+	return proxy_
+
+if __name__ == '__main__':
+	optionparser = optparse.OptionParser()
+	optionparser.add_option('-p', '--path', dest='path', help='path to the JSON-RPC server', default='/jsonrpc')
+	(options, args) = optionparser.parse_args()
+	server = main(args[0], options.path)
