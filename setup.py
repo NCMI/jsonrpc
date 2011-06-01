b728ba200289f2671568e3c1657acfe84a7fc198
Ed L <fiddlerwoa.roof+github@gmail.com>
2011-05-31 21:18:47 -0500

    Adding distutils support

---
+++ setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+
+VERSION = "0.99a"
+URLBASE = "https://github.com/NCMI/jsonrpc"
+URLMAP = {
+	"daily": "tarball/master"
+}
+
+if __name__ == "__main__":
+	setup(
+		name='jsonrpc',
+		version=VERSION,
+		description='A JSON-RPC 2.0 client-server library',
+		author='Edward Langley',
+		author_email='langleyedward@gmail.com',
+		url='https://github.com/NCMI/jsonrpc',
+		download_url="https://github.com/NCMI/jsonrpc/tarball/master",
+		packages=[
+			'jsonrpc'
+			],
+		scripts=[]
+		)
