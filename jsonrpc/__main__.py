b53e78af5aa250d0ab8a682573c1a221a070eeae
Ed L <fiddlerwoa.roof+github@gmail.com>
2011-06-18 05:25:21 +0800

    finishing previous commit

--- jsonrpc/__main__.py
+++ jsonrpc/__main__.py
@@ -1,12 +1,35 @@
+import json
 from jsonrpc import proxy
-import optparse
+import argparse
 
-def main(host, path=None):
+def main(host, path=None, args=None):
 	proxy_ = proxy.JSONRPCProxy(host, path)
 	return proxy_
 
+def iterate(iter_):
+	if not hasattr(iter_, 'next'): iter_ = iter(iter_)
+	rollback = []
+	try:
+		while True:
+			to_yield = None
+			if rollback != []:
+				to_yield = rollback.pop(0)
+			else:
+				to_yield = iter_.next()
+
+
+			result = (yield to_yield)
+			while result is not None:
+				rollback.append(result)
+				result = yield
+	except StopIteration: raise
+
 if __name__ == '__main__':
-	optionparser = optparse.OptionParser()
-	optionparser.add_option('-p', '--path', dest='path', help='path to the JSON-RPC server', default='/jsonrpc')
-	(options, args) = optionparser.parse_args()
-	server = main(args[0], options.path)
+	import sys
+	parser = argparse.ArgumentParser()
+	parser.add_argument('-p', '--path', dest='path', help='path to the JSON-RPC server', nargs='?', default='/jsonrpc')
+	parser.add_argument('host', metavar='HOST')
+	args = parser.parse_args(sys.argv[1:])
+
+	print args
+	server = main(args.host, args.path)
