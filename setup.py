7329784fb7b93cf70e49242e01950f42b40a3348
Ed L <fiddlerwoa.roof+github@gmail.com>
2011-06-17 16:20:01 -0500

    Made command line usage more sane and added some metadata to setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,9 @@ from distutils.core import setup
 VERSION = "0.99a01"
 URLBASE = "https://github.com/NCMI/jsonrpc"
 URLMAP = {
-	"daily": "tarball/master"
+	"daily": "tarball/master",
+	"0.99a01": "0.99a",
+	"0.99a02": "tarball/0.99a2",
 }
 
 if __name__ == "__main__":
@@ -14,9 +16,22 @@ if __name__ == "__main__":
 		author='Edward Langley',
 		author_email='langleyedward@gmail.com',
 		url='https://github.com/NCMI/jsonrpc',
-		download_url="https://github.com/NCMI/jsonrpc/tarball/master",
+		download_url=URLMAP.get(VERSION, URLMAP['daily']),
 		packages=[
 			'jsonrpc'
 			],
-		scripts=[]
+		scripts=[],
+		license= 'BSD 2.0',
+		keywords = ['JSON', 'jsonrpc', 'rpc'],
+		classifiers = [
+			'Development Status :: 4 - Beta',
+			'Environment :: Web Environment',
+			'Framework :: Twisted',
+			'Intended Audience :: Developers',
+			'License :: OSI Approved :: BSD License',
+			'Operating System :: OS Independent',
+			'Programming Language :: Python :: 2.6',
+			'Programming Language :: Python :: 2.7',
+			'Topic :: Software Development :: Libraries :: Python Modules',
+		]
 		)
